e56b2cd5603636e8e2996b9e0078293004f03070
Sanjar Ismailov <sanjar2008gmail.com>
2025-02-11 20:00:14 +0500

    connected index and login pages

--- falcon/urls.py
+++ falcon/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+
+from config import settings
 from . import views
+from .views import product_list, product_detail
+from django.conf.urls.static import static
 
 app_name = 'falcon'
 
 urlpatterns = [
-
-]
+    path('products/', product_list, name='product_list'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
