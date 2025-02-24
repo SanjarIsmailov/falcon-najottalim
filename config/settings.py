145f231345c808129949a4ee1e779f8db097af63
sanjar <sanjar2008gmail.com>
2025-02-24 12:17:57 +0500

    fixed & add special operations

--- config/settings.py
+++ config/settings.py
@@ -18,10 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k(v=3f=$-c_d+tecnmnez3dfhaao3mdraukn=ggm)i_m7y9ylv'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
