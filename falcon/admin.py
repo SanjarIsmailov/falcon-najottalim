9a4ccbaef89bfd7e2638c678de3dbb8a75b10e29
sanjar <sanjar2008gmail.com>
2025-02-26 19:28:25 +0500

    fixed

--- falcon/admin.py
+++ falcon/admin.py
@@ -10,7 +10,6 @@ class ProductAdmin(admin.ModelAdmin):
     ordering = ("-id",)
 
     def formatted_price(self, obj):
-        """Show original and discounted price."""
         return format_html(
             '<span style="text-decoration: line-through; color: red;">${}</span> <strong style="color: green;">${}</strong>',
             obj.original_price or 0, obj.price or 0
@@ -18,7 +17,6 @@ class ProductAdmin(admin.ModelAdmin):
     formatted_price.short_description = "Price"
 
     def admin_image(self, obj):
-        """Display product image in admin if available."""
         if obj.image:
             return format_html('<img src="{}" width="50" height="50" style="border-radius: 5px;" />', obj.image.url)
         return "No Image"
